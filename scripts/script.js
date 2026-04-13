b5bc0cb76907ca0d6af7281ef208b9dc2ffbbdf6
renatolvicente-lang <renato_l_vicente@estudante.sesisenai.org.br>
2026-04-13 11:51:04 -0300

    feat - verificação se o campo cidade está preenchido

--- scripts/script.js
+++ scripts/script.js
@@ -6,10 +6,10 @@ document.getElementById("form_doacao").addEventListener("submit", function(e){
     let email = document.getElementById("email").value;
     let idade = document.getElementById("idade").value;
     let peso = document.getElementById("peso").value;
-    
+    let cidade = document.getElementById("cidade").value;
+
     
     let data = idade.split("/");
-    
     let v_idade = data[2] - hoje;
 
     if(v_idade < 16 ) return alert("idade invalida!");
@@ -22,4 +22,6 @@ document.getElementById("form_doacao").addEventListener("submit", function(e){
     if(!email.includes("@")) return alert("Informe um email valido!");
     if(v_idade < 16) return alert("idade invalida para doação!");
     if(peso < 50) return alert("peso invalido para doação!");
+    if(cidade.length == 0) alert("Informe a cidade!!");
+
 });
