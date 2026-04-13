cf869ca397c1b295d23dfa64f0e93c5ff39d08f7
renatolvicente-lang <renato_l_vicente@estudante.sesisenai.org.br>
2026-04-13 12:58:45 -0300

    ref - reorganizando o js

--- scripts/script.js
+++ scripts/script.js
@@ -2,41 +2,48 @@ document.getElementById("form_doacao").addEventListener("submit", function(e){
     e.preventDefault();
 
 
-    let nome = document.getElementById("nome").value;
-    let email = document.getElementById("email").value;
-    let idade = document.getElementById("idade").value;
-    let peso = document.getElementById("peso").value;
-    let cidade = document.getElementById("cidade").value;
-    let estado = document.getElementById("estado").value;
-    
+    let objetos = {
+        nome: document.getElementById("nome").value,
+        email: document.getElementById("email").value,
+        idade: document.getElementById("idade").value,
+        peso: document.getElementById("peso").value,
+        cidade: document.getElementById("cidade").value,
+        estado: document.getElementById("estado").value,
+        tipo_s: document.getElementById("tipo_s").value,
+        data: idade.split("/"),
+        v_idade: data[2] - hoje,
+        data: idade.split("/"),
+        verif_nome: nome.split(" "),
+    };
 
-    let tipo_s = document.getElementById("tipo_s").value;
-
-
-    let data = idade.split("/"); //separa a data a partir do "/" e armazena no array data
-    let v_idade = data[2] - hoje;
-
-    if(v_idade < 16 ) return alert("idade invalida!");
-    
-    let verif_nome = nome.split(" "); //separa o nome a partir do " " e armazena no array verif_nome
-
-    if(verif_nome.length < 2) return alert("Nome invalido!");
-    
-
-    if(!email.includes("@")) return alert("Informe um email valido!");
-    
-    if(v_idade < 16) return alert("idade invalida para doação!");
-    
-    if(peso < 50) return alert("peso invalido para doação!");
-    
-    if(cidade.length == 0) return alert("Informe a cidade!!");
-    
-    if(estado.length == 0) return alert("Informe o estado!!"); 
-    
-    if(isNaN(telefone))return alert("Informe um telefone valido!!");
 
     
-    if(tipo_s == "" || tipo_s == "0") return alert("Selecione seu tipo sanguineo");
+
+
+    //let data = idade.split("/"); separa a data a partir do "/" e armazena no array data
+    //let v_idade = data[2] - hoje;
+
+    if(objetos.v_idade < 16 ) return alert("idade invalida!");
+    
+    //let verif_nome = nome.split(" "); separa o nome a partir do " " e armazena no array verif_nome
+
+    if(objetos.verif_nome.length < 2) return alert("Nome invalido!");
+    
+
+    if(!objetos.email.includes("@")) return alert("Informe um email valido!");
+    
+    if(objetos.v_idade < 16) return alert("idade invalida para doação!");
+    
+    if(objetos.peso < 50) return alert("peso invalido para doação!");
+    
+    if(objetos.cidade.length == 0) return alert("Informe a cidade!!");
+    
+    if(objetos.estado.length == 0) return alert("Informe o estado!!"); 
+    
+    if(isNaN(objetos.telefone))return alert("Informe um telefone valido!!");
+
+    
+    if(objetos.tipo_s == "" || objetos.tipo_s == "0") return alert("Selecione seu tipo sanguineo");
     
     let button = document.getElementById("button").addEventListener("click", function(c_color){// executa uma função que muda a cor do botão ao clicar nele
         button.style.backgroundColor = 'white';
