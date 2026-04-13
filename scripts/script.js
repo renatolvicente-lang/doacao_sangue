9e6ae3165011e63c525a21f07ac2f4b84b6e379e
renatolvicente-lang <renato_l_vicente@estudante.sesisenai.org.br>
2026-04-13 10:11:38 -0300

    feat - verificação de emails

--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,13 @@ document.getElementById("form_doacao").addEventListener("submit", function(e){
     e.preventDefault();
 
 
-    let nome = document.getElementById("nome")
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let idade = document.getElementById("idade").value;
+    let peso = document.getElementById("peso").value;
+
+
+    if(!email.includes("@")) return alert("Informe um email valido!");
+
+    
 });
