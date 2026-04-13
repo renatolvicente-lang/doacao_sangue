7523ad835d3e1ffe18f27f50aab5d48d9e9c4716
renatolvicente-lang <renato_l_vicente@estudante.sesisenai.org.br>
2026-04-13 12:39:13 -0300

    up & feat - melhoria estetica no site e adição da funcionalidade de verificar se o campo de tipo sanguineo está selecionado

--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,10 @@ document.getElementById("form_doacao").addEventListener("submit", function(e){
     let cidade = document.getElementById("cidade").value;
     let estado = document.getElementById("estado").value;
     
+
+    let tipo_s = document.getElementById("tipo_s").value;
+
+
     let data = idade.split("/"); //separa a data a partir do "/" e armazena no array data
     let v_idade = data[2] - hoje;
 
@@ -20,13 +24,19 @@ document.getElementById("form_doacao").addEventListener("submit", function(e){
     
 
     if(!email.includes("@")) return alert("Informe um email valido!");
+    
     if(v_idade < 16) return alert("idade invalida para doação!");
+    
     if(peso < 50) return alert("peso invalido para doação!");
+    
     if(cidade.length == 0) return alert("Informe a cidade!!");
+    
     if(estado.length == 0) return alert("Informe o estado!!"); 
+    
     if(!telefone === 0 )return alert("Informe um telefone valido!!");
     let button = document.getElementById("button").addEventListener("click", function(c_color){
         button.style.backgroundColor = 'white';
     });
 
+    if(tipo_s == "" || tipo_s == "0") return alert("Selecione seu tipo sanguineo");
 });
