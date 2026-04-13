89b04884a053993d56302f07afed8c9d2680f732
renatolvicente-lang <renato_l_vicente@estudante.sesisenai.org.br>
2026-04-13 11:13:29 -0300

    refactor - alteração na funcionalidade de verificação de nome

--- scripts/script.js
+++ scripts/script.js
@@ -6,12 +6,14 @@ document.getElementById("form_doacao").addEventListener("submit", function(e){
     let email = document.getElementById("email").value;
     let idade = document.getElementById("idade").value;
     let peso = document.getElementById("peso").value;
-
+    let v_idade ;
+    
     let verif_nome = nome.split(" ");
 
-    if(verif_nome[1] === "") return alert("Nome invalido!");
+    if(verif_nome.length < 2) return alert("Nome invalido!");
+    
 
     if(!email.includes("@")) return alert("Informe um email valido!");
-    if(idade < 16) return alert("idade invalida para doação!");
+    if(v_idade < 16) return alert("idade invalida para doação!");
     if(peso < 50) return alert("peso invalido para doação!");
 });
