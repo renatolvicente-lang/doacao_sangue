41a216124121bc652bcd146bfe48a197206e549a
renatolvicente-lang <renato_l_vicente@estudante.sesisenai.org.br>
2026-04-13 10:56:23 -0300

    feat - verificação de idade e verificação de peso

--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,6 @@ document.getElementById("form_doacao").addEventListener("submit", function(e){
 
 
     if(!email.includes("@")) return alert("Informe um email valido!");
-
-    
+    if(idade < 16) return alert("idade invalida para doação!");
+    if(peso < 50) return alert("peso invalido para doação!");
 });
